fe22db0890180e7bcb955c0f218a6bf4da620acf
Albert <albert.sagol@gmail.com>
2022-07-11 14:53:09 +0200

    structure of the diceselector list

---
+++ src/components/DiceSelector/DiceSelector.js
@@ -0,0 +1,12 @@
+import { listOfDice } from "../../utils/utils";
+
+const DiceSelector = () => {
+  return (
+    <>
+      <ul>{listOfDice.map((dice)=>)}</ul>
+      <button></button>
+    </>
+  );
+};
+
+export default DiceSelector;
---
+++ src/utils/utils.js
@@ -0,0 +1,7 @@
+export const listOfDice = [
+  { diceFaces: 4 },
+  { diceFaces: 6 },
+  { diceFaces: 8 },
+  { diceFaces: 10 },
+  { diceFaces: 20 },
+];
