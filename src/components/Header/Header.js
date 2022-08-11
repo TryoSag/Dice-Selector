21482faaca2bbe4ea5c0a64d0821bed13444b74b
Albert <albert.sagol@gmail.com>
2022-08-11 23:59:48 +0200

    add button historical and functions on header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,19 @@
+import { useDispatch } from "react-redux";
+import { toggleHistoricalActionCreator } from "../../redux/features/uiSlice";
 import DecorDice from "../DecorDice/DecorDice";
 import HeaderStyled from "./HeaderStyled";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const historicalButtonAction = () =>
+    dispatch(toggleHistoricalActionCreator());
+
   return (
     <HeaderStyled>
-      <DecorDice />
+      <button onClick={historicalButtonAction}>Historical</button>
       <h1>Dice Selector</h1>
+      <DecorDice />
     </HeaderStyled>
   );
 };
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,11 +1,17 @@
 import { render } from "@testing-library/react";
 import { screen } from "@testing-library/dom";
+import { Provider } from "react-redux";
 import Header from "./Header";
+import store from "../../redux/store/store";
 
 describe("Given the Header component", () => {
   describe("When it's called", () => {
     test("Then it should render a title with text 'Dice Selector'", () => {
-      render(<Header />);
+      render(
+        <Provider store={store}>
+          <Header />
+        </Provider>
+      );
 
       expect(
         screen.getByRole("heading", { name: "Dice Selector" })
